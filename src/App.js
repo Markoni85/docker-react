3f76dd37779bb8cd4918ec579ba2a0d1fb371bda
Marko Marinkovic <mmarinkovic@inovatec.ca>
2021-09-22 19:53:37 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Well Hi there, DR Strange
         </p>
         <a
           className="App-link"
